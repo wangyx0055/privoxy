6e0659cc659666e5a407b2d2ad546d70a86ac9d5
kobayosi <kobayosi4492@gmail.com>
2015-01-31 21:58:26 +0900

    imported Privoxy 3.0.23 UNRELEASED 13/01/2015

---
+++ doc/webserver/actions/step3.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+ <head>
+  <meta http-equiv="Content-Style-Type" content="text/css">
+  <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+  <link rel="stylesheet" type="text/css" href="../privoxy.css">
+  <link rel="stylesheet" type="text/css" href="../p_feedback.css">
+  <title>Out of action!</title>
+ </head>
+ <body>
+   <div class="title">
+     <h1>Out of action!</h1>
+   </div>
+   <p>
+    If you want to report a problem, please
+    <a href="http://www.privoxy.org/faq/contact.html">follow these instructions</a>.
+   </p>
+   <p>
+    Note that if your are reading this text, you are probably using an obsolete Privoxy version
+    and the problem may already be fixed in a more recent release.
+   </p>
+ </body>
+</html>
